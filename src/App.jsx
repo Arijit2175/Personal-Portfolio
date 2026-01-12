84d368820edc961dfc7cd4a3eb61426b49bde98f
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-12 20:40:08 +0530

    working on the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">App</div>
+    <div className="container mx-auto max-w-7xl">
+      {/* navbar */}
+      {/* hero */}
+      {/* about */}
+      {/* projects */}
+      {/* experience */}
+      {/* testimonials */}
+      {/* contact */}
+      {/* footer */}
+    </div>
   )
 }
 
