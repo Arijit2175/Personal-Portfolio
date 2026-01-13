908b3c36c9869c9349dd25be4fca4c643fd784e5
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-13 15:28:08 +0530

    copy function added

--- src/components/CopyEmailButton.jsx
+++ src/components/CopyEmailButton.jsx
@@ -1,6 +1,13 @@
 const CopyEmailButton = () => {
+  const [copied, setCopied] = React.useState(false);
+  const email = "arijitkarmakar2175@gmail.com";
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(email);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
   return (
-    <button className="relative px-1 py-4 text-sm text-center
+    <button onclick={copyToClipboard} className="relative px-1 py-4 text-sm text-center
     rounded-full font-extralight bg-primary w-[12rem]
     cursor-pointer overflow-hidden">
         <p className="flex-items-center justify-center gap-2">
