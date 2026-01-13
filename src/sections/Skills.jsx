db86e8545df6f83b08f5dc5cefe795c423a6a4eb
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-13 19:37:51 +0530

    returning skill card component

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
-const Skills = () => {
+const SkillCard = ({ img, name, username, body }) => {
   return (
-    <div>Skills</div>
-  )
-}
+    <figure
+      className={twMerge(
+        "relative h-full w-64 cursor-pointer overflow-hidden rounded-xl border p-4 border-gray-50/[.1] bg-gradient-to-r bg-indigo to-storm hover:bg-royal hover-animation"
+      )}
+    >
+      <div className="flex flex-row items-center gap-2">
+        <img
+          className="rounded-full bg-white/10"
+          width="32"
+          height="32"
+          alt=""
+          src={img}
+        />
+        <div className="flex flex-col">
+          <figcaption className="text-sm font-medium text-white">
+            {name}
+          </figcaption>
+          <p className="text-xs font-medium text-white/40">{username}</p>
+        </div>
+      </div>
+      <blockquote className="mt-2 text-sm">{body}</blockquote>
+    </figure>
+  );
+};
+
+
 
 export default Skills
