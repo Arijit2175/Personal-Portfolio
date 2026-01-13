a1a0652fe8c3980e06f8b7115abe77bb51b108cd
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-13 15:24:58 +0530

    created the button

---
+++ src/components/CopyEmailButton.jsx
@@ -0,0 +1,14 @@
+const CopyEmailButton = () => {
+  return (
+    <button className="relative px-1 py-4 text-sm text-center
+    rounded-full font-extralight bg-primary w-[12rem]
+    cursor-pointer overflow-hidden">
+        <p className="flex-items-center justify-center gap-2">
+            <img src="assets/copy.svg" className="w-5" alt="copyicon"/>
+            Copy Email Address
+        </p>
+    </button>
+  );
+};
+
+export default CopyEmailButton
