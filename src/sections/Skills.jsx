53b00ca90e3104444e7615cd078a9e2b10a6969e
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-13 19:39:51 +0530

    exporting the function

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -25,6 +25,24 @@ const SkillCard = ({ img, name, username, body }) => {
   );
 };
 
-
-
-export default Skills
+export default function Skills() {
+  return (
+    <div className="items-start mt-25 md:mt-35 c-space">
+      <h2 className="text-heading">My Skill Levels</h2>
+      <div className="relative flex flex-col items-center justify-center w-full mt-12 overflow-hidden">
+        <Marquee pauseOnHover className="[--duration:20s]">
+          {firstRow.map((skill) => (
+            <SkillCard key={skill.username} {...skill} />
+          ))}
+        </Marquee>
+        <Marquee reverse pauseOnHover className="[--duration:20s]">
+          {secondRow.map((skill) => (
+            <SkillCard key={skill.username} {...skill} />
+          ))}
+        </Marquee>
+        <div className="absolute inset-y-0 left-0 w-1/4 pointer-events-none bg-gradient-to-r from-primary"></div>
+        <div className="absolute inset-y-0 right-0 w-1/4 pointer-events-none bg-gradient-to-l from-primary"></div>
+      </div>
+    </div>
+  );
+}
