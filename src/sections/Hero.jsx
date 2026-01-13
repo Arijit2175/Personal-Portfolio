3be64e63044054346455d0184b761035b0a42a01
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-13 12:25:41 +0530

    for parallax bg

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,11 @@
+import HeroText from "../components/HeroText"
+
 const Hero = () => {
   return <section className="flex items-start 
   justify-center md:items-start md:justify-start 
-  min-h-screen overflow-hidden c-space"></section>
+  min-h-screen overflow-hidden c-space">
+    <HeroText />
+    </section>
 }
 
 export default Hero
