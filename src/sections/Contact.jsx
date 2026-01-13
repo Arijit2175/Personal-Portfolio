62a2b586a57f852d7893c6bf35caacbad4816274
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-13 20:41:06 +0530

    adding functions for handling submission

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,19 +1,50 @@
 const Contact = () => {
-  return <section className="relative flex items-center c-space
-  section-spacing">
-    <div className="flex flex-col items-center justify-center
-    max-w-md p-5 mx-auto border border-white/10 rounded-2xl
-    bg-primary">
-        <div className="flex flex-col items-start w-full gap-5
-        mb-10">
-            <h2 className="text-heading">Contact Me</h2>
-            <p className="font-normal text-neutral-400">
-                Contact me if you are looking for collaboration for creating new projects or working 
-                on existing projects together. I'm here to help.
-            </p>
-        </div>
-    </div>
-  </section>
-}
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertType, setAlertType] = useState("success");
+  const [alertMessage, setAlertMessage] = useState("");
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const showAlertMessage = (type, message) => {
+    setAlertType(type);
+    setAlertMessage(message);
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 5000);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      console.log("From submitted:", formData);
+      await emailjs.send(
+        "service_79b0nyj",
+        "template_17us8im",
+        {
+          from_name: formData.name,
+          to_name: "Ali",
+          from_email: formData.email,
+          to_email: "AliSanatiDev@gmail.com",
+          message: formData.message,
+        },
+        "pn-Bw_mS1_QQdofuV"
+      );
+      setIsLoading(false);
+      setFormData({ name: "", email: "", message: "" });
+      showAlertMessage("success", "You message has been sent!");
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+      showAlertMessage("danger", "Somthing went wrong!");
+    }
+  };
 
 export default Contact
