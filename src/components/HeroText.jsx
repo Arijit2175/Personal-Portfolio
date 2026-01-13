3575870aa8aed9cbb02a45bd34dbf228f5c13b29
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-13 12:10:53 +0530

    checking the hero section

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -2,6 +2,15 @@ const HeroText = () => {
   return <div className="z-10 mt-20 text-center md:mt-40
   md:text-left rounded-3xl bg-clip-text">
     {/* DesktopView */}
+    <div className="flex-col hidden md:flex c-space">
+      <h1 className="text-4xl font-medium">Hi I'm Arijit</h1>
+      <div className="flex flex-col items-start">
+        <p className="text-5xl font-medium text-neutral-300">
+          A Developer <br /> Dedicated to Crafting
+          </p>
+          {/* get flip words */}
+      </div>
+    </div>
     {/* MobileView */}
   </div>
 }
