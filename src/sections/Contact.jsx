5c713d0a52434f7b76cc47d74c4a609ce8f069e9
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-13 20:59:09 +0530

    made a change hidden the emailjs keys

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -26,16 +26,16 @@ const Contact = () => {
     try {
       console.log("From submitted:", formData);
       await emailjs.send(
-        "service_79b0nyj",
-        "template_17us8im",
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         {
           from_name: formData.name,
-          to_name: "Ali",
+          to_name: "Arijit",
           from_email: formData.email,
-          to_email: "AliSanatiDev@gmail.com",
+          to_email: "arijitkarmakar2175@gmail.com",
           message: formData.message,
         },
-        "pn-Bw_mS1_QQdofuV"
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       );
       setIsLoading(false);
       setFormData({ name: "", email: "", message: "" });
@@ -46,5 +46,78 @@ const Contact = () => {
       showAlertMessage("danger", "Somthing went wrong!");
     }
   };
+
+   return (
+    <section className="relative flex items-center c-space section-spacing">
+        {showAlert && <Alert type={alertType} text={alertMessage} />}
+      <div className="flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary">
+        <div className="flex flex-col items-start w-full gap-5 mb-10">
+          <h2 className="text-heading">Let's Talk</h2>
+          <p className="font-normal text-neutral-400">
+            Contact me if you need help in creating digital projects or need help in 
+            existing projects. I'm here to help.
+          </p>
+        </div>
+        <form className="w-full" onSubmit={handleSubmit}>
+          <div className="mb-5">
+            <label htmlFor="name" className="feild-label">
+              Full Name
+            </label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              className="field-input field-input-focus"
+              placeholder="John Doe"
+              autoComplete="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="email" className="feild-label">
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              className="field-input field-input-focus"
+              placeholder="JohnDoe@email.com"
+              autoComplete="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="message" className="feild-label">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              type="text"
+              rows="4"
+              className="field-input field-input-focus"
+              placeholder="Share your thoughts..."
+              autoComplete="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full px-1 py-3 text-lg text-center rounded-md cursor-pointer bg-radial from-lavender to-royal hover-animation"
+          >
+            {!isLoading ? "Send" : "Sending..."}
+          </button>
+        </form>
+      </div>
+    </section>
+   );
+}
 
 export default Contact
