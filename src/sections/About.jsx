565d3a0f425141d8f3e423173e1e662aad0ddedf
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-13 14:51:54 +0530

    worked on grid 1

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,10 +1,23 @@
 const About = () => {
   return (
-    <section className="c-space section-spacing">
+    <section className="c-space section-spacing" id="about">
         <h2 className="text-heading">About Me</h2>
         <div className="grid grid-cols-1 gap-4  md:grid-cols-6 md:auto-rows-[18rem] mt-12">
             {/* Grid 1 */}
-            <div className="flex items-end grid-default-color grid-1"></div>
+            <div className="flex items-end grid-default-color grid-1">
+                <img
+            src="assets/coding-pov.jpeg"
+            className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
+          />
+          <div className="z-10">
+            <p className="headtext">Hi, I'm Arijit Karmakar</p>
+            <p className="subtext">
+              Working on developing modernistic and responsive web applications
+              that provide excellent user experiences.
+            </p>
+          </div>
+          <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
+            </div>
             {/* Grid 2 */}
             <div className="grid-default-color grid-2"></div>
             {/* Grid 3 */}
