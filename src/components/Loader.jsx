1a5b026f8b8388cf3f45ff69bd429665b4787d7c
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-13 14:34:08 +0530

    written loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loader = () => {
+  const { progress } = useProgress();
+  return (
+    <Html center className="text-xl font-normal text-center">
+      {progress}% Loaded
+    </Html>
+  );
+};
+
+export default Loader;
