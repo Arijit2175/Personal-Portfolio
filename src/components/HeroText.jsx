deca3c9bb9edf1d5cb7c390cbbe914dfccbcab50
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-01-14 11:01:44 +0530

    made changes for the navbar

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -2,7 +2,7 @@ import { motion } from "motion/react";
 import { FlipWords} from "./FlipWords";
 
 const HeroText = () => {
-  const words = ["Secure", "Modern", "Scalable"];
+  const words = ["Designed", "Modern", "Improved", "Efficient", "Scalable"];
   const variants = {
     hidden: { opacity: 0, x: -50 },
     visible: { opacity: 1, x: 0 },
@@ -28,7 +28,7 @@ const HeroText = () => {
             animate="visible"
             transition={{ delay: 1.2 }}
           >
-            A Developer <br /> Dedicated to Crafting
+            A Developer <br /> Dedicated to Creating
           </motion.p>
           <motion.div
             variants={variants}
@@ -48,7 +48,7 @@ const HeroText = () => {
             animate="visible"
             transition={{ delay: 1.8 }}
           >
-            Web Solutions
+            Solutions
           </motion.p>
         </div>
       </div>
